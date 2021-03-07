ca7e49235d9e7c8165e1c628e43f7603852bbe91
LalithK90 <asakahatapitiya@gmail.com>
2021-03-08 01:36:07 +0530

    discount ratio was added

---
+++ src/main/java/lk/sampath_autocare/asset/discount_ratio/entity/DiscountRatio.java
@@ -0,0 +1,39 @@
+package lk.sampath_autocare.asset.discount_ratio.entity;
+
+
+import com.fasterxml.jackson.annotation.JsonFilter;
+import lk.sampath_autocare.asset.common_asset.model.Enum.LiveDead;
+import lk.sampath_autocare.asset.payment.entity.Payment;
+import lk.sampath_autocare.util.audit.AuditEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.List;
+
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonFilter( "DiscountRatio" )
+public class DiscountRatio extends AuditEntity {
+
+    @Column( nullable = false, length = 45 )
+    private String name;
+
+    @Column( nullable = false, precision = 10, scale = 2 )
+    private BigDecimal amount;
+
+    @Enumerated( EnumType.STRING)
+    private LiveDead liveDead;
+
+    @OneToMany( mappedBy = "discountRatio" )
+    private List< Payment > payments;
+
+}
+
