340f6a5d328f4314a781dd43125bd34f9d92802d
LalithK90 <asakahatapitiya@gmail.com>
2021-03-24 22:40:13 +0530

    sec disable

--- src/main/java/lk/sampath_autocare/configuration/SecurityConfig.java
+++ src/main/java/lk/sampath_autocare/configuration/SecurityConfig.java
@@ -71,11 +71,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-//            http.csrf().disable();
-//            http.authorizeRequests().antMatchers("/").permitAll();
+            http.csrf().disable();
+            http.authorizeRequests().antMatchers("/").permitAll();
     // For developing easy to give permission all lin
 
-    http
+   /* http
         .authorizeRequests(
             authorizeRequests ->
                 authorizeRequests
@@ -122,7 +122,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .sessionRegistry(sessionRegistry()))
         //Cross site disable
         .csrf(AbstractHttpConfigurer::disable)
-        .exceptionHandling();
+        .exceptionHandling();*/
   }
 }
 
